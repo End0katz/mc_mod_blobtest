0f1662118fb5be1510dc0b529f36bc24dd74c9e6
End0katz <endokatz@gmail.com>
2024-12-27 16:41:49 +0000

    Adding support for adding items (not actually adding items) Also added logger framework for init

--- src/main/java/com/end0katz/blobtest/Blobtest.java
+++ src/main/java/com/end0katz/blobtest/Blobtest.java
@@ -5,9 +5,11 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+
 public class Blobtest implements ModInitializer {
 	public static final String MOD_ID = "blobtest";
-
+	public static HashMap<String, Boolean> LoadedThings = new HashMap<>();
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
@@ -18,9 +20,22 @@ public class Blobtest implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+		LoadedThings.putIfAbsent("Items:start", false);
+		LoadedThings.putIfAbsent("Items:finish", false);
+
+		try{Thread.sleep(10000L);} catch (InterruptedException e) {
+			LOGGER.info("InterruptedExecution whilst sleeping: %s".formatted(e.getMessage()));
+		}
 
 		LOGGER.info("------- Blobtest Initializing progress: -------");
 		LOGGER.info(" # Logger");
+		for (String i: LoadedThings.keySet()){
+			if (LoadedThings.get(i)){
+				LOGGER.info(" # %s".formatted(i));
+			} else {
+				LOGGER.warn(" WARNING: %s did not load within 10 seconds".formatted(i));
+			}
+		}
 		LOGGER.info("------- Blobtest Initializing completed -------");
 	}
 }
---
+++ src/main/java/com/end0katz/blobtest/item/ModItems.java
@@ -0,0 +1,10 @@
+package com.end0katz.blobtest.item;
+
+import com.end0katz.blobtest.Blobtest;
+
+public class ModItems {
+    public static void registerModItems() {
+        Blobtest.LoadedThings.put("Items:start", true);
+        Blobtest.LoadedThings.put("Items:finish", true);
+    }
+}
