dfda6bd27e9565e05441e3f567d41d6532dfb819
End0katz <endokatz@gmail.com>
2024-12-27 16:58:37 +0000

    Added blobium ingot/nugget items (no texture, creative tab or name). To access these, use Items.x, or items.x.ITEM/ID/SETTINGS

--- src/main/java/com/end0katz/blobtest/item/ModItems.java
+++ src/main/java/com/end0katz/blobtest/item/ModItems.java
@@ -3,7 +3,7 @@ package com.end0katz.blobtest.item;
 import com.end0katz.blobtest.Blobtest;
 
 public class ModItems {
-    public static void registerModItems() {
+    public static void register() {
         Blobtest.LoadedThings.put("Items:start", true);
         Blobtest.LoadedThings.put("Items:finish", true);
     }
